0e470c03a1dcefac511040198acd6c49aafa98cd
Mona Lisa <125455854+swervolee@users.noreply.github.com>
2024-02-03 11:00:08 +0300

    Status api

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,9 +2,24 @@
 """index file"""
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
 
 
 @app_views.route("/status")
 def status():
     """returns server status"""
     return jsonify({"status": "OK"})
+
+
+@app_views.route("/stats")
+def stats():
+    """RETURNS THE OBJECTS STATS COUNT"""
+    result = {'amenities': storage.count("Amenity"),
+              'cities': storage.count("City"),
+              'places': storage.count("Place"),
+              'reviews': storage.count("Review"),
+              'states': storage.count("State"),
+              'users': storage.count("User")
+              }
+
+    return jsonify(result)
