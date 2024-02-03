3fac8e78fb89186f3f500af20cef0e8bdbb89ddb
Mona Lisa <125455854+swervolee@users.noreply.github.com>
2024-02-03 17:44:29 +0300

    Update

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,49 +1,30 @@
 #!/usr/bin/python3
-"""STATES API"""
-from api.v1.views import app_views
+"""FLASK APP"""
+
+
+from os import getenv
+from flask import Flask, Blueprint, jsonify
 from models import storage
-from models.state import State
-from flask import jsonify, abort, request
+from api.v1.views import app_views
+
+app = Flask(__name__)
+app.register_blueprint(app_views)
 
 
-@app_views.route("/states", methods=['GET', 'POST'], strict_slashes=False)
-def states_fetch():
-    """GETS ALL STATES"""
-    if request.method == 'POST':
-        data = request.get_json()
-        if data is not None:
-            if 'name' not in data:
-                abort(400, "Missing name")
-            new = State(**data)
-            storage.new(new)
-            storage.save()
-            return jsonify(new.to_dict()), 201
-        abort(400, "Not  a JSON")
-    elif request.method == "GET":
-        states = storage.all("State").values()
-        return jsonify([obj.to_dict() for obj in states])
+@app.teardown_appcontext
+def teardown_appcontext(cmd):
+    """CLOSES CURRENT SESSION"""
+    storage.close()
 
 
-@app_views.route("/states/<state_id>",
-                 methods=["GET", "DELETE", "PUT"], strict_slashes=False)
-def state_by_id(state_id=""):
-    """GETS A STATE BY ID AND OR DELETES IT OR UPDATES IT"""
-    result = [v for k, v in storage.all("State").
-              items() if k.split(".")[1] == state_id]
-    if result == []:
-        abort(404)
-    if request.method == "DELETE":
-        storage.delete(result[0])
-        storage.save()
-        return jsonify({}), 200
-    elif request.method == 'GET':
-        return jsonify(result[0].to_dict())
-    elif request.method == "PUT":
-        data = request.get_json()
-        if data is not None:
-            for key, value in data.items():
-                if key not in ['id', 'created_at', 'updated_at']:
-                    setattr(result[0], key, value)
-            result[0].save()
-            return jsonify(result[0].to_dict()), 200
-        abort(400, "Not a  JSON")
+@app.errorhandler(404)
+def error_404(error):
+    """HANDLES PAGE NOT FOUND(404)"""
+    return jsonify({'error': 'Not found'}), 404
+
+
+if __name__ == "__main__":
+    host = getenv("HBNB_API_HOST", "0.0.0.0")
+    port = int(getenv("HBNB_API_PORT", 5000))
+
+    app.run(host=host, port=port, threaded=True)
