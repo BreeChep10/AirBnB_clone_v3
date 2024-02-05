cedd8697880e18cb8ddf04a3419ae9542d37a479
Mona Lisa <125455854+swervolee@users.noreply.github.com>
2024-02-06 00:48:06 +0300

    .

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -62,3 +62,72 @@ def get_place(place_id=""):
             result[0].save()
             return jsonify(result[0].to_dict()), 200
         abort(400, "Not a JSON")
+
+
+@app_views.route("/places_search", methods=["POST"], strict_slashes=False)
+def place_search():
+    """A PLACE SEARCH"""
+    all_cities = []
+
+    data = request.get_json()
+
+    if not data:
+        abort(400, "Not a JSON")
+
+    states_ids = data.get("states")
+    cities_ids = data.get("cities")
+    amenity_ids = data.get("amenities")
+
+    if not states_ids and not cities_ids:
+        all = [v for k, v in storage.all("Place").items()]
+        if amenity_ids and getenv('HBNB_STORAGE_TYPE') == 'db':
+            for item in all:
+                item_amenities = [obj.id for obj in item.amenities]
+                for i in amenity_ids:
+                    if i not in item_amenities:
+                        all.remove(item)
+                        break
+            return jsonify([obj.to_dict() for obj in all])
+        elif amenity_ids:
+            for item in all:
+                for i in amenity_ids:
+                    if i not in item.amenity_ids:
+                        all.remove(item)
+                        break
+            return jsonify([obj.to_dict() for obj in all])
+        else:
+            return jsonify([obj.to_dict() for obj in all])
+
+    if states_ids and states_ids != []:
+        states = [v for k, v in storage.all("State").
+                  items() if k.split(".")[1] in states_ids]
+        for obj in states:
+            for item in obj.cities:
+                all_cities.append(item)
+
+    if cities_ids and cities_ids != []:
+        cities = [v for k, v in storage.all("City").
+                  items() if k.split(".")[1] in cities_ids]
+        for obj in cities:
+            if obj not in all_cities:
+                all_cities.append(obj)
+
+    all_places = [x for y in all_cities for x in y.places]
+
+    if amenity_ids and getenv("HBNB_STORAGE_TYPE") == 'db':
+        for k, item in enumerate(all_places):
+            item_amenity_id = [x.id for x in item.amenities]
+            for i in amenity_ids:
+                if i not in item_amenity_id:
+                    del all_places[k]
+                    break
+        return jsonify([item.to_dict() for item in all_places])
+    elif amenity_ids:
+        for k, item in enumerate(all_places):
+            for i in amenity_ids:
+                if i not in item.amenity_ids:
+                    del all_places[k]
+                    break
+        return jsonify([obj.to_dict() for obj in all_places])
+    else:
+        return jsonify([obj.to_dict() for obj in all_places])
